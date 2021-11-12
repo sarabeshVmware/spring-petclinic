cf9bc873bac15b072009f5667d904c2fd0475352
kinjal dhaduk <38214481+dhadukk@users.noreply.github.com>
2021-11-12 14:57:09 +0530

    Removed awk commands for Getting external IP

--- tap-tests/e2e/ip.go
+++ tap-tests/e2e/ip.go
@@ -4,32 +4,62 @@
 package e2e
 
 import (
+	"context"
+	"fmt"
 	"log"
-	"strings"
-
-	tap "gitlab.eng.vmware.com/tap/tap-packaging-tests/pkg"
+	"os"
+	"path/filepath"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
 func GetAppAcceleratorExternalIP() string {
 	log.Printf("Getting app accelerator external IP:")
-	appAccExternalIPBytes, _ := tap.RunWithBash(`kubectl get svc -A | awk '{if($2=="acc-ui-server")print $5}'`)
-	appAccExternalIP := strings.TrimSpace(string(appAccExternalIPBytes))
+	// appAccExternalIPBytes, _ := tap.RunWithBash(`kubectl get svc -A | awk '{if($2=="acc-ui-server")print $5}'`)
+	// appAccExternalIP := strings.TrimSpace(string(appAccExternalIPBytes))
+	appAccExternalIP := GetServiceExternalIp("acc-ui-server", "accelerator-system")
 	log.Printf("App Accelerator external IP: %s", appAccExternalIP)
 	return appAccExternalIP
 }
 
 func GetAppLiveViewExternalIP() string {
 	log.Printf("Getting app live view external IP:")
-	appLiveViewExternalIPBytes, _ := tap.RunWithBash(`kubectl get svc -A | awk '{if($2=="application-live-view-5112")print $5}'`)
-	appLiveViewExternalIP := strings.TrimSpace(string(appLiveViewExternalIPBytes))
+	// appLiveViewExternalIPBytes, _ := tap.RunWithBash(`kubectl get svc -A | awk '{if($2=="application-live-view-5112")print $5}'`)
+	// appLiveViewExternalIP := strings.TrimSpace(string(appLiveViewExternalIPBytes))
+	appLiveViewExternalIP := GetServiceExternalIp("application-live-view-5112", "app-live-view")
 	log.Printf("App Live View external IP: %s", appLiveViewExternalIP)
 	return appLiveViewExternalIP
 }
 
 func GetEnvoyExternalIP() string {
 	log.Printf("Getting envoy external IP:")
-	envoyExternalIPBytes, _ := tap.RunWithBash(`kubectl get svc -n contour-external | awk '{if($1=="envoy")print $4}'`)
-	envoyExternalIP := strings.TrimSpace(string(envoyExternalIPBytes))
+	// envoyExternalIPBytes, _ := tap.RunWithBash(`kubectl get svc -n contour-external | awk '{if($1=="envoy")print $4}'`)
+	// envoyExternalIP := strings.TrimSpace(string(envoyExternalIPBytes))
+	envoyExternalIP := GetServiceExternalIp("envoy", "contour-external")
 	log.Printf("Envoy external IP: %s", envoyExternalIP)
 	return envoyExternalIP
+}
+
+func GetServiceExternalIp(serviceName string, namespace string) string {
+	kubeconfig := filepath.Join(
+		os.Getenv("HOME"), ".kube", "config",
+	)
+	var externalIp string
+	config, err := clientcmd.BuildConfigFromFlags("", kubeconfig)
+	if err != nil {
+		log.Println("BuildConfigFromFlags Failed")
+	}
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		log.Println("NewForConfig Failed")
+	}
+	svcList, err := clientset.CoreV1().Services(namespace).List(context.TODO(), metav1.ListOptions{
+		FieldSelector: "metadata.name=" + serviceName,
+	})
+	for _, svc := range svcList.Items {
+		externalIp = svc.Status.LoadBalancer.Ingress[0].IP
+		break
+	}
+	return externalIp
 }
