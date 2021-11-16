bf661d89bdc077a20a80ea6c84941e2dff5e0d30
kinjal dhaduk <38214481+dhadukk@users.noreply.github.com>
2021-11-16 15:41:45 +0530

    Innerloop wf staging

--- tap-tests/e2e/accelerator.go
+++ tap-tests/e2e/accelerator.go
@@ -20,11 +20,15 @@ func ListAccelerators() {
 	}
 }
 
-func GenerateAcceleratorProject(label string, projectName string, repositoryPrefix string, unzip bool) {
+func GenerateAcceleratorProject(label string, projectName string, repositoryPrefix string, unzip bool, serverIP string) {
 	log.Printf("Generating accelerator project: %s", label)
-	tap.RunWithBash(fmt.Sprintf(`tanzu accelerator generate %s --options '{"projectName":"%s", "repositoryPrefix":"%s", "includeKubernetes": true}'`, label, projectName, repositoryPrefix))
+	tap.RunWithBash(fmt.Sprintf(`tanzu accelerator generate %s --options '{"projectName":"%s", "repositoryPrefix":"%s", "includeKubernetes": true}' --server-url http://%s`, label, projectName, repositoryPrefix, serverIP))
 	if unzip {
-		tap.Run(fmt.Sprintf("rm -r %s", projectName))
+		out, err := tap.Run_AllowError(fmt.Sprintf("ls -lt %s", projectName))
+		if err == nil{
+			tap.Run(fmt.Sprintf("rm -r %s", projectName))
+		}
+		log.Printf("Output: \n%s", string(out))
 		tap.Run(fmt.Sprintf("unzip %s.zip", projectName))
 	}
 }
--- tap-tests/e2e/innerloop/innerloop.go
+++ tap-tests/e2e/innerloop/innerloop.go
@@ -8,8 +8,11 @@ import (
 	"log"
 	"os"
 	"path/filepath"
-	"time"
+	//"time"
 
+	//"github.com/mitchellh/mapstructure"
+	//"reflect"
+	// "github.com/docker/cli/cli/command/registry"
 	tap "gitlab.eng.vmware.com/tap/tap-packaging-tests/pkg"
 	tapInstall "gitlab.eng.vmware.com/tap/tap-packaging-tests/tap-install/tapinstall"
 	e2e "gitlab.eng.vmware.com/tap/tap-packaging-tests/tap-tests/e2e"
@@ -22,18 +25,8 @@ func InnerloopSourceBuildDeploy(installPackages bool, preCleanup bool, postClean
 	valuesDir := tapInstall.GetDefaultValuesDir()
 
 	config := tapInstall.GetConfig(configFile, valuesDir)
-
-	defaultSupplyChainSchema := struct {
-		Registry struct {
-			Repository string `yaml:"repository"`
-			Server     string `yaml:"server"`
-		} `yaml:"registry"`
-	}{}
-	defaultSupplyChainSchemaBytes, err := os.ReadFile(filepath.Join(valuesDir, "default-supply-chain.yaml"))
-	tap.CheckError(err)
-	err = yaml.Unmarshal(defaultSupplyChainSchemaBytes, &defaultSupplyChainSchema)
-	tap.CheckError(err)
-
+	scRepository, scServer := GetSCRegistryDetails(valuesDir)
+	
 	acceleratorProject := "tanzu-java-web-app"
 	workload := "tanzu-java-web-app"
 	sourceImage := fmt.Sprintf("%s-src", workload)
@@ -44,28 +37,35 @@ func InnerloopSourceBuildDeploy(installPackages bool, preCleanup bool, postClean
 		tapInstall.Install(configFile, valuesDir, preCleanup, false)
 	}
 
-	tap.RunWithBash(`ps aux | grep -i kubectl | grep -v grep | awk {'print $2'} | xargs kill`)
-	pidAppAcceleratorPortForward, _ := tap.RunAndDisown("kubectl -n accelerator-system port-forward svc/acc-ui-server 8877:80")
-	time.Sleep(5 * time.Second)
-	defer tap.KillPID(pidAppAcceleratorPortForward)
-	pidAppLiveViewPortForward, _ := tap.RunAndDisown("kubectl -n tap-install port-forward service/application-live-view-5112 5112:5112")
-	time.Sleep(5 * time.Second)
-	defer tap.KillPID(pidAppLiveViewPortForward)
+	// tap.RunWithBash(`ps aux | grep -i kubectl | grep -v grep | awk {'print $2'} | xargs kill`)
+	// pidAppAcceleratorPortForward, _ := tap.RunAndDisown("kubectl -n accelerator-system port-forward svc/acc-ui-server 8877:80")
+	// time.Sleep(5 * time.Second)
+	// defer tap.KillPID(pidAppAcceleratorPortForward)
+	// pidAppLiveViewPortForward, _ := tap.RunAndDisown("kubectl -n tap-install port-forward service/application-live-view-5112 5112:5112")
+	// time.Sleep(5 * time.Second)
+	// defer tap.KillPID(pidAppLiveViewPortForward)
 
-	e2e.GetAppAcceleratorExternalIP()
+	appAccExternalIP := e2e.GetAppAcceleratorExternalIP()
+
+	// Setting Env variable ACC_SERVER_URL
+	tap.RunWithBash(fmt.Sprintf("export ACC_SERVER_URL=http://%s" , appAccExternalIP))
+
+
 	e2e.GetAppLiveViewExternalIP()
 
 	e2e.ListAccelerators()
-	e2e.GenerateAcceleratorProject(acceleratorProject, acceleratorProject, defaultSupplyChainSchema.Registry.Server, true)
-
+	e2e.GenerateAcceleratorProject(acceleratorProject, acceleratorProject, scServer, true, appAccExternalIP)
+	
+	tap.SetupDeveloperNamespacePostInstallation(config.Namespace)
 	e2e.DeleteWorkload(workload, config.Namespace)
-	e2e.CreateWorkload(workload, defaultSupplyChainSchema.Registry.Server, defaultSupplyChainSchema.Registry.Repository, sourceImage, acceleratorProject, config.Namespace)
+	e2e.CreateWorkload(workload, scServer, scRepository, sourceImage, acceleratorProject, config.Namespace)
+	
 
 	verify(workload, config.Namespace, oldString, newString, false)
 
 	e2e.UpdateFile("tanzu-java-web-app/src/main/java/com/example/springboot/HelloController.java", oldString, newString)
 	e2e.UpdateFile("tanzu-java-web-app/src/test/java/com/example/springboot/HelloControllerTest.java", oldString, newString)
-	e2e.UpdateWorkload(workload, defaultSupplyChainSchema.Registry.Server, defaultSupplyChainSchema.Registry.Repository, sourceImage, acceleratorProject, config.Namespace)
+	e2e.UpdateWorkload(workload, scServer, scRepository, sourceImage, acceleratorProject, config.Namespace)
 
 	verify(workload, config.Namespace, oldString, newString, true)
 
@@ -83,3 +83,52 @@ func verify(workload string, namespace string, oldString string, newString strin
 	e2e.VerifyWorkloadStatus(workload, namespace)
 	e2e.VerifyApplicationRunningWithValidationString(e2e.GetEnvoyExternalIP(), "tanzu-java-web-app.tap-install.example.com", oldString, newString, testNew)
 }
+
+func GetSCRegistryDetails(valuesDir string)(string, string){
+	type supplyChainSchema struct{
+		SupplyChain      string `yaml:"supply_chain"`
+		OotbSupplyChainBasic struct {
+			Registry struct {
+				Server     string `yaml:"server"`
+				Repository string `yaml:"repository"`
+			} `yaml:"registry"`
+		} `yaml:"ootb_supply_chain_basic"`
+		OotbSupplyChainTesting struct {
+			Registry struct {
+				Server     string `yaml:"server"`
+				Repository string `yaml:"repository"`
+			} `yaml:"registry"`
+		} `yaml:"ootb_supply_chain_testing"`
+		OotbSupplyChainTestingScanning struct {
+			Registry struct {
+				Server     string `yaml:"server"`
+				Repository string `yaml:"repository"`
+			} `yaml:"registry"`
+		} `yaml:"ootb_supply_chain_testing_scanning"`}
+		var scSchema supplyChainSchema
+		var repository string
+		var server string
+		
+		supplyChainSchemaBytes, err := os.ReadFile(filepath.Join(valuesDir, "tap-values.yaml"))
+		tap.CheckError(err)
+		err = yaml.Unmarshal(supplyChainSchemaBytes, &scSchema)
+		tap.CheckError(err)
+		if scSchema.SupplyChain == "" || scSchema.SupplyChain =="basic"{
+			scregistry := scSchema.OotbSupplyChainBasic.Registry
+			repository = scregistry.Repository
+			server = scregistry.Server
+		}else if scSchema.SupplyChain =="testing"{
+			scregistry := scSchema.OotbSupplyChainTesting.Registry
+			repository = scregistry.Repository
+			server = scregistry.Server
+		}else if scSchema.SupplyChain == "scanning"{
+			scregistry := scSchema.OotbSupplyChainTestingScanning.Registry
+			repository = scregistry.Repository
+			server = scregistry.Server
+		}else{
+			log.Println("Invalid Supply chain schema in values.yaml file")
+		}
+		
+		fmt.Print(repository, server)
+	return repository, server
+} 
