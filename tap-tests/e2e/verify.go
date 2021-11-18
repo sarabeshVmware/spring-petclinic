958474c412cbc0ab9df6077757044d1467e6766b
kinjal dhaduk <38214481+dhadukk@users.noreply.github.com>
2021-11-18 19:03:03 +0530

    Format code

--- tap-tests/e2e/verify.go
+++ tap-tests/e2e/verify.go
@@ -3,7 +3,6 @@
 
 package e2e
 
-
 import (
 	"fmt"
 	"io/ioutil"
@@ -11,10 +10,13 @@ import (
 	"net/http"
 	"strings"
 	"time"
+
 	"github.com/pivotal/kpack/pkg/apis/build/v1alpha2"
 	tap "gitlab.eng.vmware.com/tap/tap-packaging-tests/pkg"
+
 	//util "gitlab.eng.vmware.com/tap/tap-packaging-tests/pkg/util"
 	"context"
+
 	corev1 "k8s.io/api/core/v1"
 )
 
@@ -33,59 +35,59 @@ func VerifyImageRepositoryReadyStatus(name string, namespace string) {
 	}
 }
 
-// func verifyBuildsAreGenerated(namespace string) {
-// 	log.Printf("Checking if builds are generated in namespace: %s", namespace)
-// 	buildsBytes, _ := tap.Run(fmt.Sprintf("kubectl get build -n %s", namespace))
-// 	builds := strings.TrimSpace(string(buildsBytes))
-// 	if builds == fmt.Sprintf("No resources found in %s namespace.", namespace) {
-// 		log.Printf("Builds not generated in namespace: %s", namespace)
-// 		time.Sleep(5 * time.Second)
-// 		verifyBuildsAreGenerated(namespace)
-// 	} else {
-// 		log.Printf("Builds generated in namespace: %s", namespace)
-// 	}
-// }
+func verifyBuildsAreGenerated(namespace string) {
+	log.Printf("Checking if builds are generated in namespace: %s", namespace)
+	buildsBytes, _ := tap.Run(fmt.Sprintf("kubectl get build -n %s", namespace))
+	builds := strings.TrimSpace(string(buildsBytes))
+	if builds == fmt.Sprintf("No resources found in %s namespace.", namespace) {
+		log.Printf("Builds not generated in namespace: %s", namespace)
+		time.Sleep(5 * time.Second)
+		verifyBuildsAreGenerated(namespace)
+	} else {
+		log.Printf("Builds generated in namespace: %s", namespace)
+	}
+}
 
-// func verifyBuildIsNotInUnknownState(buildName string, namespace string) {
-// 	log.Printf("Checking if build is in unknown state: %s", buildName)
-// 	buildStateBytes, _ := tap.RunWithBash(fmt.Sprintf(`kubectl get build -n %s | awk '{if($1=="%s")print $2}'`, namespace, buildName))
-// 	buildState := strings.TrimSpace(string(buildStateBytes))
-// 	if buildState == "Unknown" {
-// 		log.Printf("Build in unknown state: %s", buildName)
-// 		time.Sleep(5 * time.Second)
-// 		verifyBuildIsNotInUnknownState(buildName, namespace)
-// 	} else {
-// 		log.Printf("Build not in unknown state: %s", buildName)
-// 		log.Printf("Build image: %s", buildState)
-// 	}
-// }
+func verifyBuildIsNotInUnknownState(buildName string, namespace string) {
+	log.Printf("Checking if build is in unknown state: %s", buildName)
+	buildStateBytes, _ := tap.RunWithBash(fmt.Sprintf(`kubectl get build -n %s | awk '{if($1=="%s")print $2}'`, namespace, buildName))
+	buildState := strings.TrimSpace(string(buildStateBytes))
+	if buildState == "Unknown" {
+		log.Printf("Build in unknown state: %s", buildName)
+		time.Sleep(5 * time.Second)
+		verifyBuildIsNotInUnknownState(buildName, namespace)
+	} else {
+		log.Printf("Build not in unknown state: %s", buildName)
+		log.Printf("Build image: %s", buildState)
+	}
+}
 
-// func VerifyBuildStatus(pattern string, namespace string, verifyBuildsGeneration bool) {
-// 	log.Printf("Checking build status for pattern: %s", pattern)
-// 	if verifyBuildsGeneration {
-// 		verifyBuildsAreGenerated(namespace)
-// 	}
+func VerifyBuildStatusDepricated(pattern string, namespace string, verifyBuildsGeneration bool) {
+	log.Printf("Checking build status for pattern: %s", pattern)
+	if verifyBuildsGeneration {
+		verifyBuildsAreGenerated(namespace)
+	}
 
-// 	log.Printf("Getting build names matching pattern: %s", pattern)
-// 	buildNamesBytes, _ := tap.RunWithBash(fmt.Sprintf(`kubectl get build -n %s | awk '{if($1~"%s")print $1}' | xargs`, namespace, pattern))
-// 	buildNames := strings.Split(strings.TrimSpace(string(buildNamesBytes)), " ")
-// 	allBuildsFailed := true
-// 	for _, buildName := range buildNames {
-// 		log.Printf("Checking build status for build: %s", buildName)
-// 		verifyBuildIsNotInUnknownState(buildName, namespace)
-// 		buildStateBytes, _ := tap.RunWithBash(fmt.Sprintf(`kubectl get build -n %s | awk '{if($1=="%s")print $3}'`, namespace, buildName))
-// 		buildState := strings.TrimSpace(string(buildStateBytes))
-// 		log.Printf("Build %s status: %s", buildName, buildState)
-// 		if buildState == "True" {
-// 			log.Printf("Build ready: %s", buildName)
-// 			allBuildsFailed = false
-// 			break
-// 		}
-// 	}
-// 	if allBuildsFailed {
-// 		log.Fatalf("No builds ready for the pattern: %s", pattern)
-// 	}
-// }
+	log.Printf("Getting build names matching pattern: %s", pattern)
+	buildNamesBytes, _ := tap.RunWithBash(fmt.Sprintf(`kubectl get build -n %s | awk '{if($1~"%s")print $1}' | xargs`, namespace, pattern))
+	buildNames := strings.Split(strings.TrimSpace(string(buildNamesBytes)), " ")
+	allBuildsFailed := true
+	for _, buildName := range buildNames {
+		log.Printf("Checking build status for build: %s", buildName)
+		verifyBuildIsNotInUnknownState(buildName, namespace)
+		buildStateBytes, _ := tap.RunWithBash(fmt.Sprintf(`kubectl get build -n %s | awk '{if($1=="%s")print $3}'`, namespace, buildName))
+		buildState := strings.TrimSpace(string(buildStateBytes))
+		log.Printf("Build %s status: %s", buildName, buildState)
+		if buildState == "True" {
+			log.Printf("Build ready: %s", buildName)
+			allBuildsFailed = false
+			break
+		}
+	}
+	if allBuildsFailed {
+		log.Fatalf("No builds ready for the pattern: %s", pattern)
+	}
+}
 
 func VerifyKnativeServiceStatus(name string, namespace string) {
 	log.Printf("Checking knative service status for: %s", name)
@@ -146,40 +148,37 @@ func VerifyApplicationRunningWithValidationString(envoyExternalIP string, host s
 	}
 }
 
-func VerifyBuildStatus(){
+func VerifyBuildStatus() {
 	count := 30
-	for count <= 30{
-		if count == 0{
+	for count <= 30 {
+		if count == 0 {
 			log.Fatalf("Builds are not generated after 5 mins")
 			break
 		}
 		result := GetBuilds()
-		if len(result.Items) != 0{
-			latestBuildIndex :=  len(result.Items) - 1 
+		if len(result.Items) != 0 {
+			latestBuildIndex := len(result.Items) - 1
 			lastConditionIndex := len(result.Items[0].Status.Status.Conditions) - 1
 			//Expect(result.Items[0].Status.Status.Conditions[lastConditionIndex].Status).To(Equal(corev1.ConditionTrue))
-			if (result.Items[latestBuildIndex].Status.Status.Conditions[lastConditionIndex].Status) == corev1.ConditionUnknown{
+			if (result.Items[latestBuildIndex].Status.Status.Conditions[lastConditionIndex].Status) == corev1.ConditionUnknown {
 				log.Printf("Build %s status is Unknown", result.Items[latestBuildIndex].ObjectMeta.Name)
 				//time.Sleep(5 * time.Second)
-				
-			} else if (result.Items[latestBuildIndex].Status.Status.Conditions[lastConditionIndex].Status) == corev1.ConditionTrue{
-				log.Printf("Build %s status is verified successfully. Status is %s", result.Items[latestBuildIndex].ObjectMeta.Name, result.Items[latestBuildIndex].Status.Status.Conditions[lastConditionIndex].Status)
+
+			} else if (result.Items[latestBuildIndex].Status.Status.Conditions[lastConditionIndex].Status) == corev1.ConditionTrue {
+				log.Printf("Build %s status is verified successfully. Status is %s", result.Items[latestBuildIndex].ObjectMeta.Name,
+				 result.Items[latestBuildIndex].Status.Status.Conditions[lastConditionIndex].Status)
 				break
 			}
-		} else{
+		} else {
 			log.Println("Builds are not generated yet")
 		}
 		log.Printf("Waiting for 10s for builds getting generated ...")
 		time.Sleep(10 * time.Second)
 		count -= 1
 	}
-	
-	
-	
 
-		
 }
-func GetBuilds() v1alpha2.BuildList{
+func GetBuilds() v1alpha2.BuildList {
 	var restClient = tap.GetRestClient()
 	result := v1alpha2.BuildList{}
 	request := restClient.Get()
