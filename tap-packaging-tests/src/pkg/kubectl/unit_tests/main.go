a702bd2cc9ab0e8e468bdb60e0e28d3aafe55702
Madhuri Dogra <mdogra@mdogra-a01.vmware.com>
2022-01-03 23:54:00 +0530

    sample helper usage

--- tap-packaging-tests/src/pkg/kubectl/unit_tests/main.go
+++ tap-packaging-tests/src/pkg/kubectl/unit_tests/main.go
@@ -6,25 +6,11 @@ import (
 )
 
 func main() {
-	kubectl_helper.ValidateAppLiveViewLabels()
-	kubectl_helper.GetImageRepositoryStatus("tanzu-java-web-app", "tap-install")
 
 	// Examples of kubectl linux cmd ouput parsing
 
-	a := kubectl_helper.GetLatestImage("tap-install")
+	a := kubectl_helper.GetLatestImageStatus("tap-install")
 	fmt.Printf("%+v\n", a)
-
-	b := kubectl_helper.GetPodintent("tanzu-java-web-app", "tap-install")
-	fmt.Printf("podIntents: %+v\n", b)
-
-	c := kubectl_helper.GetWorkload("tanzu-java-web-app", "tap-install")
-	fmt.Printf("%+v\n", c)
-
-	d := kubectl_helper.GetImageRepositories("tanzu-java-web-app", "tap-install")
-	fmt.Printf("%+v\n", d)
-
-	e := kubectl_helper.GetBuilds("tanzu-java-web-app-build-1", "tap-install")
-	fmt.Printf("%+v\n", e)
 
 	// Examples of kubectl json output parsing
 
