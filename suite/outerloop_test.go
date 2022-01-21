31e600dbb67350b3f2715354a9a65b81ec91c1fa
Divyanshu Madan <divyanshum@vmware.com>
2022-01-21 09:15:40 +0530

    add features; use stepfuncs module

--- suite/outerloop_test.go
+++ suite/outerloop_test.go
@@ -3,12 +3,10 @@ package suite
 import (
 	"context"
 	"fmt"
-	"time"
 
 	"testing"
 
-	"gitlab.eng.vmware.com/tap/tap-packages/suite/client"
-	"gitlab.eng.vmware.com/tap/tap-packages/suite/exec"
+	"gitlab.eng.vmware.com/tap/tap-packages/suite/stepfuncs"
 	"sigs.k8s.io/e2e-framework/pkg/envconf"
 	"sigs.k8s.io/e2e-framework/pkg/features"
 )
@@ -18,207 +16,136 @@ func TestOuterloopBasic(t *testing.T) {
 		Assess("update-schema", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
 			tapValuesSchema.Profile = "full"
 			tapValuesSchema.SupplyChain = "basic"
-
-			t.Logf("updating tap values schema %s", config.Tap.ValuesSchemaFile)
-			err := WriteYAMLFile(config.Tap.ValuesSchemaFile, tapValuesSchema)
-			if err != nil {
-				t.Error(fmt.Errorf("error while updating tap values schema %s: %w", config.Tap.ValuesSchemaFile, err))
-				t.FailNow()
-			}
-			t.Logf("tap values schema %s updated", config.Tap.ValuesSchemaFile)
-			return ctx
+			return stepfuncs.WriteFile(ctx, t, cfg, config.Tap.ValuesSchemaFile, tapValuesSchema)
 		}).
 		Assess("update-tap", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
-			t.Logf("updating package %s", config.Tap.Name)
-			cmd, output, err := exec.TanzuUpdatePackage(config.Tap.Name, config.Tap.PackageName, config.Tap.Version, config.Tap.Namespace, config.Tap.ValuesSchemaFile)
-			t.Logf("command executed: %s", cmd)
-			if err != nil {
-				t.Error(fmt.Errorf("error while updating package %s: %w: %s", config.Tap.Name, err, output))
-				t.FailNow()
-			}
-			t.Logf("package %s updated: %s", config.Tap.Name, output)
-			return ctx
+			return stepfuncs.UpdatePackage(ctx, t, cfg, config.Tap.Name, config.Tap.PackageName, config.Tap.Version, config.Tap.Namespace, config.Tap.ValuesSchemaFile)
 		}).
 		Feature()
 
-	tapGuiServerExternalIpKey, tapGuiServerPortKey := "tapGuiServerExternalIp", "tapGuiServerPort"
+	// tapGuiServerExternalIpKey, tapGuiServerPortKey := "tapGuiServerExternalIp", "tapGuiServerPort"
 
-	f2 := features.New("get-tapgui-server-externalip-port").
-		Assess("get-externalip", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
-			service, tapGuiNamespace := "server", "tap-gui"
+	// f2 := features.New("get-tapgui-server-externalip-port").
+	// 	Assess("get-externalip", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+	// 		ctx, serviceExternalIp := stepfuncs.GetServiceExternalIp(ctx, t, cfg, "server", "tap-gui")
+	// 		return context.WithValue(ctx, tapGuiServerExternalIpKey, serviceExternalIp)
+	// 	}).
+	// 	Assess("get-port", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+	// 		ctx, serviceExternalPort := stepfuncs.GetServiceExternalPort(ctx, t, cfg, "server", "tap-gui")
+	// 		return context.WithValue(ctx, tapGuiServerPortKey, serviceExternalPort)
+	// 	}).
+	// 	Feature()
 
-			t.Logf("getting external ip for %s (namespace %s)", service, tapGuiNamespace)
-			serviceExternalIp, err := client.GetServiceExternalIP(service, tapGuiNamespace, cfg.Client().RESTConfig())
-			if err != nil {
-				t.Error(fmt.Errorf("error while getting external ip for %s (namespace %s): %w", service, tapGuiNamespace, err))
-				t.FailNow()
-			}
-			t.Logf("external ip for %s (namespace %s): %s", "server", tapGuiNamespace, serviceExternalIp)
-			return context.WithValue(ctx, tapGuiServerExternalIpKey, serviceExternalIp)
-		}).
-		Assess("get-port", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
-			service, tapGuiNamespace := "server", "tap-gui"
-
-			t.Logf("getting port for %s (namespace %s)", service, tapGuiNamespace)
-			servicePort, err := client.GetServicePort(service, tapGuiNamespace, cfg.Client().RESTConfig())
-			if err != nil {
-				t.Error(fmt.Errorf("error while getting port for %s (namespace %s): %w", service, tapGuiNamespace, err))
-				t.FailNow()
-			}
-			t.Logf("port for %s (namespace %s): %d", service, tapGuiNamespace, servicePort)
-			return context.WithValue(ctx, tapGuiServerPortKey, servicePort)
-		}).
-		Feature()
-
-	f3 := features.New("update-tap-tapgui-schema").
-		Assess("update-schema", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
-			baseUrl := ctx.Value(tapGuiServerExternalIpKey).(string) + ":" + fmt.Sprint(ctx.Value(tapGuiServerPortKey).(int))
-			appTitle, catalogType := "TAP", "url"
-
-			tapValuesSchema.TapGui.AppConfig.App.Title = appTitle
-			tapValuesSchema.TapGui.AppConfig.App.BaseURL = baseUrl
-			tapValuesSchema.TapGui.AppConfig.Backend.BaseURL = baseUrl
-			tapValuesSchema.TapGui.AppConfig.Backend.Cors.Origin = baseUrl
-			tapValuesSchema.TapGui.AppConfig.Catalog.Locations = make([]struct {
-				Target string "yaml:\"target,omitempty\""
-				Type   string "yaml:\"type,omitempty\""
-			}, 1)
-			tapValuesSchema.TapGui.AppConfig.Catalog.Locations[0].Type = catalogType
-			tapValuesSchema.TapGui.AppConfig.Catalog.Locations[0].Target = config.Outerloop.CatalogInfoYaml
-
-			t.Logf("updating tap values schema %s", config.Tap.ValuesSchemaFile)
-			err := WriteYAMLFile(config.Tap.ValuesSchemaFile, tapValuesSchema)
-			if err != nil {
-				t.Error(fmt.Errorf("error while updating tap values schema %s: %w", config.Tap.ValuesSchemaFile, err))
-				t.FailNow()
-			}
-			t.Logf("tap values schema %s updated", config.Tap.ValuesSchemaFile)
-			return ctx
-		}).
-		Assess("update-tap", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
-			t.Logf("updating package %s", config.Tap.Name)
-			cmd, output, err := exec.TanzuUpdatePackage(config.Tap.Name, config.Tap.PackageName, config.Tap.Version, config.Tap.Namespace, config.Tap.ValuesSchemaFile)
-			t.Logf("command executed: %s", cmd)
-			if err != nil {
-				t.Error(fmt.Errorf("error while updating package %s: %w: %s", config.Tap.Name, err, output))
-				t.FailNow()
-			}
-			t.Logf("package %s updated: %s", config.Tap.Name, output)
-			return ctx
-		}).
-		Feature()
+	// f3 := features.New("update-tap-tapgui-schema").
+	// 	Assess("update-schema", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+	// 		baseUrl := ctx.Value(tapGuiServerExternalIpKey).(string) + ":" + fmt.Sprint(ctx.Value(tapGuiServerPortKey).(int))
+	// 		appTitle, catalogType := "TAP", "url"
+	// 		tapValuesSchema.TapGui.AppConfig.App.Title = appTitle
+	// 		tapValuesSchema.TapGui.AppConfig.App.BaseURL = baseUrl
+	// 		tapValuesSchema.TapGui.AppConfig.Backend.BaseURL = baseUrl
+	// 		tapValuesSchema.TapGui.AppConfig.Backend.Cors.Origin = baseUrl
+	// 		tapValuesSchema.TapGui.AppConfig.Catalog.Locations = make([]struct {
+	// 			Target string "yaml:\"target,omitempty\""
+	// 			Type   string "yaml:\"type,omitempty\""
+	// 		}, 1)
+	// 		tapValuesSchema.TapGui.AppConfig.Catalog.Locations[0].Type = catalogType
+	// 		tapValuesSchema.TapGui.AppConfig.Catalog.Locations[0].Target = config.Outerloop.CatalogInfoYaml
+	// 		return stepfuncs.WriteFile(ctx, t, cfg, config.Tap.ValuesSchemaFile, tapValuesSchema)
+	// 	}).
+	// 	Assess("update-tap", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+	// 		return stepfuncs.UpdatePackage(ctx, t, cfg, config.Tap.Name, config.Tap.PackageName, config.Tap.Version, config.Tap.Namespace, config.Tap.ValuesSchemaFile)
+	// 	}).
+	// 	Feature()
 
 	f4 := features.New("deploy-apps-via-yaml-configurations").
 		Assess("deploy-springpetclinic-pipeline", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
-			t.Logf("deploying app %s in namespace %s", config.Outerloop.SpringPetclinic.Name, config.Outerloop.SpringPetclinic.Namespace)
-			cmd, output, err := exec.KappDeployAppInNamespace(config.Outerloop.SpringPetclinic.Name, []string{config.Outerloop.SpringPetclinic.YamlFile}, config.Outerloop.SpringPetclinic.Namespace)
-			t.Logf("command executed: %s", cmd)
-			if err != nil {
-				t.Error(fmt.Errorf("error while deploying app %s in namespace %s: %w: %s", config.Outerloop.SpringPetclinic.Name, config.Outerloop.SpringPetclinic.Namespace, err, output))
-				t.FailNow()
-			}
-			t.Logf("app %s deployed in namespace %s: %s", config.Outerloop.SpringPetclinic.Name, config.Outerloop.SpringPetclinic.Namespace, output)
-			return ctx
+			return stepfuncs.DeployAppInNamespace(ctx, t, cfg, config.Outerloop.SpringPetclinic.Name, []string{config.Outerloop.SpringPetclinic.YamlFile}, config.Outerloop.SpringPetclinic.Namespace)
 		}).
 		Assess("deploy-mysqldb", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
-			t.Logf("deploying app %s in namespace %s", config.Outerloop.Mysql.Name, config.Outerloop.Mysql.Namespace)
-			cmd, output, err := exec.KappDeployAppInNamespace(config.Outerloop.Mysql.Name, []string{config.Outerloop.Mysql.YamlFile}, config.Outerloop.Mysql.Namespace)
-			t.Logf("command executed: %s", cmd)
-			if err != nil {
-				t.Error(fmt.Errorf("error while deploying app %s in namespace %s: %w: %s", config.Outerloop.Mysql.Name, config.Outerloop.Mysql.Namespace, err, output))
-				t.FailNow()
-			}
-			t.Logf("app %s deployed in namespace %s: %s", config.Outerloop.Mysql.Name, config.Outerloop.Mysql.Namespace, output)
-			return ctx
+			return stepfuncs.DeployAppInNamespace(ctx, t, cfg, config.Outerloop.Mysql.Name, []string{config.Outerloop.Mysql.YamlFile}, config.Outerloop.Mysql.Namespace)
 		}).
 		Feature()
 
 	f5 := features.New("patch-default-serviceaccount").
 		Assess("patch-imagepullsecrets", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
-			serviceAccount := "default"
-
-			t.Logf("patching imagepullsecrets %s, %s to service account %s", config.TanzunetCredsSecret.Name, config.ImageSecret.Name, serviceAccount)
-			cmd, output, err := exec.KubectlPatchServiceAccount(serviceAccount, config.Outerloop.Namespace, fmt.Sprintf(`'{"imagePullSecrets": [{"name": "%s"}, {"name": "%s"}]}'`, config.TanzunetCredsSecret.Name, config.ImageSecret.Name))
-			t.Logf("command executed: %s", cmd)
-			if err != nil {
-				t.Error(fmt.Errorf("error while patching imagepullsecrets %s, %s to service account %s: %w: %s", config.TanzunetCredsSecret.Name, config.ImageSecret.Name, serviceAccount, err, output))
-				t.FailNow()
-			}
-			t.Logf("imagepullsecrets %s, %s patched to service account %s: %s", config.TanzunetCredsSecret.Name, config.ImageSecret.Name, serviceAccount, output)
-			return ctx
+			return stepfuncs.PatchServiceAccount(ctx, t, cfg, fmt.Sprintf(`'{"imagePullSecrets": [{"name": "%s"}, {"name": "%s"}]}'`, config.TanzunetCredsSecret.Name, config.ImageSecret.Name), "default", config.Outerloop.Namespace)
 		}).
 		Assess("patch-secrets", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
-			serviceAccount := "default"
-
-			t.Logf("patching secret %s to service account %s", config.ImageSecret.Name, serviceAccount)
-			cmd, output, err := exec.KubectlPatchServiceAccount(serviceAccount, config.Outerloop.Namespace, fmt.Sprintf(`'{"secrets": [{"name": "%s"}]}'`, config.ImageSecret.Name))
-			t.Logf("command executed: %s", cmd)
-			if err != nil {
-				t.Error(fmt.Errorf("error while patching secret %s to service account %s: %w: %s", config.ImageSecret.Name, serviceAccount, err, output))
-				t.FailNow()
-			}
-			t.Logf("secret %s patched to service account %s: %s", config.ImageSecret.Name, serviceAccount, output)
-			return ctx
+			return stepfuncs.PatchServiceAccount(ctx, t, cfg, fmt.Sprintf(`'{"secrets": [{"name": "%s"}]}'`, config.ImageSecret.Name), "default", config.Outerloop.Namespace)
 		}).
 		Feature()
 
 	f6 := features.New("create-clusterrolebinding").
 		Assess("create-clusterrolebinding", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
-			name, clusterRole, serviceAccount := "apps-admin", "cluster-admin", fmt.Sprintf("%s:default", config.Outerloop.Namespace)
-
-			t.Logf("creating clusterrolebinding %s for clusterrole %s and serviceaccount %s", name, clusterRole, serviceAccount)
-			cmd, output, err := exec.KubectlCreateClusterRoleBinding(name, clusterRole, serviceAccount)
-			t.Logf("command executed: %s", cmd)
-			if err != nil {
-				t.Error(fmt.Errorf("error while creating cluster role binding %s for clusterrole %s and serviceaccount %s: %w: %s", name, clusterRole, serviceAccount, err, output))
-				t.FailNow()
-			}
-			t.Logf("clusterrolebinding %s created for clusterrole %s and serviceaccount %s: %s", name, clusterRole, serviceAccount, output)
-			return ctx
+			return stepfuncs.CreateClusterRoleBinding(ctx, t, cfg, "apps-admin", "cluster-admin", fmt.Sprintf("%s:default", config.Outerloop.Namespace))
 		}).
 		Feature()
 
 	f7 := features.New("deploy-workload").
 		Assess("deploy-workload", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
-			t.Logf("deploying workload %s in namespace %s", config.Outerloop.Workload.YamlFile, config.Outerloop.Workload.Namespace)
-			cmd, output, err := exec.TanzuDeployWorkload(config.Outerloop.Workload.YamlFile, config.Outerloop.Workload.Namespace)
-			t.Logf("command executed: %s", cmd)
-			if err != nil {
-				t.Error(fmt.Errorf("error while deploying workload %s in namespace %s: %w: %s", config.Outerloop.Workload.YamlFile, config.Outerloop.Workload.Namespace, err, output))
-				t.FailNow()
-			}
-			t.Logf("workload %s deployed in namespace %s: %s", config.Outerloop.Workload.YamlFile, config.Outerloop.Workload.Namespace, output)
-			return ctx
+			return stepfuncs.DeployWorkload(ctx, t, cfg, config.Outerloop.Workload.YamlFile, config.Outerloop.Workload.Namespace)
 		}).
 		Feature()
 
 	f8 := features.New("verify-imagerepository-status").
-		Assess("verify-imagerepository", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
-			ready := false
-			iter := 30
-			for i := 1; i <= iter; i++ {
-				t.Logf("getting image repository status for %s in namespace %s (iteration %d)", config.Outerloop.SpringPetclinic.ImageRepositoryName, config.Outerloop.Workload.Namespace, i)
-				cmd, output, err := exec.KubectlGetImageRepositoryStatus(config.Outerloop.SpringPetclinic.ImageRepositoryName, config.Outerloop.Workload.Namespace)
-				t.Logf("command executed: %s", cmd)
-				if err != nil {
-					t.Error(fmt.Errorf("error while getting image repository status for %s in namespace %s: %w: %s", config.Outerloop.SpringPetclinic.ImageRepositoryName, config.Outerloop.Workload.Namespace, err, output))
-					t.FailNow()
-				}
-				t.Logf("workload status for %s in namespace %s: %s", config.Outerloop.SpringPetclinic.ImageRepositoryName, config.Outerloop.Workload.Namespace, output)
-				if output == "Ready" {
-					ready = true
-					break
-				}
-				t.Logf("sleeping for 1 minute")
-				time.Sleep(time.Minute)
-			}
-			if !ready {
-				t.Errorf(`image repository failed to get into ready state after %d iterations`, iter)
-				t.Fail()
-			}
-			return ctx
+		Assess("verify-imagerepository-ready", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+			return stepfuncs.VerifyImagerepositoryReady(ctx, t, cfg, config.Outerloop.SpringPetclinic.ImagerepositoryName, config.Outerloop.Workload.Namespace)
 		}).
 		Feature()
 
-	testenv.Test(t, f1, f2, f3, f4, f5, f6, f7, f8)
+	f9 := features.New("verify-gitrepo-status").
+		Assess("verify-gitrepo-ready", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+			return stepfuncs.VerifyGitrepoReady(ctx, t, cfg, config.Outerloop.SpringPetclinic.GitrepositoryName, config.Outerloop.Workload.Namespace)
+		}).
+		Feature()
+
+	f10 := features.New("verify-build-status").
+		Assess("verify-build-succeeded", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+			return stepfuncs.VerifyBuildSucceeded(ctx, t, cfg, config.Outerloop.SpringPetclinic.BuildNamePrefix, config.Outerloop.Workload.Namespace)
+		}).
+		Feature()
+
+	f11 := features.New("verify-podintent-status").
+		Assess("verify-podintent-ready", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+			return stepfuncs.VerifyPodintentReady(ctx, t, cfg, config.Outerloop.SpringPetclinic.PodintentName, config.Outerloop.Workload.Namespace)
+		}).
+		Feature()
+
+	f12 := features.New("verify-podintent-annotations").
+		Assess("verify-applied-conventions", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+			return stepfuncs.VerifyPodintentAnnotation(ctx, t, cfg, "conventions.apps.tanzu.vmware.com/applied-conventions", "", true, config.Outerloop.SpringPetclinic.PodintentName, config.Outerloop.Workload.Namespace)
+		}).
+		Assess("verify-developer-conventions", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+			return stepfuncs.VerifyPodintentAnnotation(ctx, t, cfg, "developer.conventions/target-containers", "workload", false, config.Outerloop.SpringPetclinic.PodintentName, config.Outerloop.Workload.Namespace)
+		}).
+		Feature()
+
+	f13 := features.New("verify-podintent-labels").
+		Assess("verify-appliveview", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+			return stepfuncs.VerifyPodintentLabel(ctx, t, cfg, "tanzu.app.live.view", "true", false, config.Outerloop.SpringPetclinic.PodintentName, config.Outerloop.Workload.Namespace)
+		}).
+		Assess("verify-appliveview-applicatoin-flavours", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+			return stepfuncs.VerifyPodintentLabel(ctx, t, cfg, "tanzu.app.live.view.application.flavours", "spring-boot", false, config.Outerloop.SpringPetclinic.PodintentName, config.Outerloop.Workload.Namespace)
+		}).
+		Assess("verify-appliveview-application-name", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+			return stepfuncs.VerifyPodintentLabel(ctx, t, cfg, "tanzu.app.live.view.application.name", "petclinic", false, config.Outerloop.SpringPetclinic.PodintentName, config.Outerloop.Workload.Namespace)
+		}).
+		Assess("verify-springboot-conventions", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+			return stepfuncs.VerifyPodintentLabel(ctx, t, cfg, "conventions.apps.tanzu.vmware.com/framework", "spring-boot", false, config.Outerloop.SpringPetclinic.PodintentName, config.Outerloop.Workload.Namespace)
+		}).
+		Feature()
+
+	f14 := features.New("verify-ksvc-status").
+		Assess("verify-ksvc-ready", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+			return stepfuncs.VerifyKsvcReady(ctx, t, cfg, config.Outerloop.SpringPetclinic.KsvcName, config.Outerloop.Workload.Namespace)
+		}).
+		Feature()
+
+	f15 := features.New("verify-taskrun-status").
+		Assess("verify-taskrun-succeeded", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+			return stepfuncs.VerifyTaskrunSucceeded(ctx, t, cfg, config.Outerloop.SpringPetclinic.TaskrunNamePrefix, config.Outerloop.Workload.Namespace)
+		}).
+		Feature()
+
+	testenv.Test(t, f1, f4, f5, f6, f7, f8, f9, f10, f11, f12, f13, f14, f15)
 }
