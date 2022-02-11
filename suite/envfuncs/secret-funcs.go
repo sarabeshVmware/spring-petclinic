5568814ec0c0084bd6aa936787901bd1e034618a
dapdelivery <svc.dapdelivery@gmail.com>
2022-02-11 14:34:36 +0530

    removed exec module use

--- suite/envfuncs/secret-funcs.go
+++ suite/envfuncs/secret-funcs.go
@@ -8,33 +8,35 @@ import (
 	"fmt"
 	"log"
 
-	"gitlab.eng.vmware.com/tap/tap-packages/suite/exec"
+	"gitlab.eng.vmware.com/tap/tap-packages/suite/pkg/tanzu/tanzuCmds"
 	"sigs.k8s.io/e2e-framework/pkg/env"
 	"sigs.k8s.io/e2e-framework/pkg/envconf"
 )
 
 func CreateSecret(name string, registry string, username string, password string, namespace string, export bool) env.Func {
 	return func(ctx context.Context, cfg *envconf.Config) (context.Context, error) {
-		log.Printf("creating secret %s", name)
-		_, output, err := exec.TanzuCreateSecret(name, registry, username, password, namespace, export)
-		// don't log command as it contains password
+		log.Printf("creating secret %s (registry %s, username %s) in namespace %s", name, registry, username, namespace)
+
+		// create secret
+		err := tanzuCmds.TanzuCreateSecret(name, registry, username, password, namespace, export)
 		if err != nil {
-			return ctx, fmt.Errorf("error while creating secret %s: %w: %s", name, err, output)
+			return ctx, fmt.Errorf("error while creating secret %s (registry %s, username %s) in namespace %s", name, registry, username, namespace)
 		}
-		log.Printf("secret %s created: %s", name, output)
+
 		return ctx, nil
 	}
 }
 
 func DeleteSecret(name string, namespace string) env.Func {
 	return func(ctx context.Context, cfg *envconf.Config) (context.Context, error) {
-		log.Printf("deleting secret %s", name)
-		cmd, output, err := exec.TanzuDeleteSecret(name, namespace)
-		log.Printf("command executed: %s", cmd)
+		log.Printf("deleting secret %s from namespace %s", name, namespace)
+
+		// delete secret
+		err := tanzuCmds.TanzuDeleteSecret(name, namespace)
 		if err != nil {
-			return ctx, fmt.Errorf("error while deleting secret %s: %w: %s", name, err, output)
+			return ctx, fmt.Errorf("error while deleting secret %s from namespace %s", name, namespace)
 		}
-		log.Printf("secret %s deleted: %s", name, output)
+
 		return ctx, nil
 	}
 }
