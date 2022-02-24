de56a11a59a435cbd3d73d1daddc28868c260e9e
dapdelivery <svc.dapdelivery@gmail.com>
2022-02-24 14:39:33 +0530

    Moved imagerepository-delivery verification at last

--- suite/innerloop_test.go
+++ suite/innerloop_test.go
@@ -543,7 +543,7 @@ func TestInnerloopBasic(t *testing.T) {
 			return ctx
 		}).
 		Feature()
-	testenv.Test(t, f1, f2, f3, f4, f5, f6, f7, f8, f9, f10, f11, f12, f13, f14, f15, f16, cleanup)
+	testenv.Test(t, f1, f2, f3, f4, f5, f6, f7, f8, f9, f10, f17, f11, f12, f13, f14, f15, f16, cleanup)
 
 	t.Log("************** TestCase END: TestInnerloopBasic **************")
 }
