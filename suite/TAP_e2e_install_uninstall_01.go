680488904b23cd576520a20293aa49aa36ac0fc0
Madhuri Dogra <mdogra@mdogra-a01.vmware.com>
2022-03-10 14:18:58 +0530

    first cut code

---
+++ suite/TAP_e2e_install_uninstall_01.go
@@ -0,0 +1,99 @@
+//go:build all || outerloop || outerloop_basic_delivery
+
+package suite
+
+import (
+	"context"
+	"io/ioutil"
+	"os"
+	"testing"
+
+	"gitlab.eng.vmware.com/tap/tap-packages/suite/pkg/kubectl/kubectl_helpers"
+	"gitlab.eng.vmware.com/tap/tap-packages/suite/pkg/tanzu/tanzuCmds"
+	"gitlab.eng.vmware.com/tap/tap-packages/suite/pkg/tanzu/tanzu_libs"
+	"gitlab.eng.vmware.com/tap/tap-packages/suite/pkg/utils"
+	"sigs.k8s.io/e2e-framework/pkg/envconf"
+	"sigs.k8s.io/e2e-framework/pkg/features"
+)
+
+type Packages []struct {
+	Name       string `yaml:"name"`
+	Package    string `yaml:"package"`
+	ValuesFile string `yaml:"values_file,omitempty"`
+	RbacFile   string `yaml:"rbac_file,omitempty"`
+	PollTimout string `yaml:"poll_timout,omitempty"`
+}
+
+var PackagesResourcesDir = filepath.Join(utils.GetFileDir(), "resources", "components")
+
+func getPackagesList() (Packages, error) {
+	log.Printf("getting package list")
+
+	pkgList := Packages{}
+	file := filepath.Join(PackagesResourcesDir, "packages.yaml")
+
+	// read file
+	pkgListBytes, err := os.ReadFile(file)
+	if err != nil {
+		log.Printf("error while reading packages file %s", file)
+		log.Printf("error: %s", err)
+		return pkgList, err
+	} else {
+		log.Printf("read packages file %s", file)
+	}
+
+	// unmarshall
+	err = yaml.Unmarshal(pkgListBytes, &pkgList)
+	if err != nil {
+		log.Printf("error while unmarshalling packages file %s", file)
+		log.Printf("error: %s", err)
+		return pkgList, err
+	} else {
+		log.Printf("unmarshalled file %s", file)
+	}
+
+	return pkgList, nil
+}
+
+
+func TestInstallUninstallAllComponentAllVersionInPackageRepo(t *testing.T) {
+	t.Log("************** TestCase START: TestInstallUninstallAllComponentAllVersionInPAckageRepo **************")
+
+	var pkgList, _ = getPackagesList()
+	for _, pkg := pkgList	{
+		log.Printf("package : %s", pkg.Name)
+		//fetch versions n list them first
+		f1 := features.New("install-uninstall-cert-manager").
+			Assess("install-component", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+				kubectl_helpers.GetWorkloadStatus("")
+				return ctx
+			}).
+			Assess("update-tap", func(ctx context.Context, t *testing.T, cfg *envconf.Config) context.Context {
+				t.Logf("updating package %s", suiteConfig.Tap.Name)
+				cmd, output, err := exec.TanzuUpdatePackage(suiteConfig.Tap.Name, suiteConfig.Tap.PackageName, suiteConfig.Tap.Version, suiteConfig.Tap.Namespace, suiteConfig.Tap.ValuesSchemaFile)
+				t.Logf("command executed: %s", cmd)
+				if err != nil {
+					t.Error(fmt.Errorf("error while updating package %s: %w: %s", suiteConfig.Tap.Name, err, output))
+					t.FailNow()
+				}
+				t.Logf("package %s updated: %s", suiteConfig.Tap.Name, output)
+				t.Logf("sleeping for 1 minute")
+				time.Sleep(time.Minute)
+				return ctx
+		}).
+		Feature()
+	}
+
+	for _, pkg := pkgList	{
+		// tetst here
+		// write feature for each pkg last version uninstall
+	}
+
+	//loop and install uninstall pkg components
+
+	// test
+	testenv.Test(t,
+		TestInstallUninstallAllComponentAllVersionInPackageRepo,
+	)
+	t.Log("************** TestCase END: TestInstallUninstallAllComponentAllVersionInPAckageRepo **************")
+}
