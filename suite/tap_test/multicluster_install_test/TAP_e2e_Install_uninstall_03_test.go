185d2ce53ff3edb854e7e30464145409276c5a18
dapdelivery <svc.dapdelivery@gmail.com>
2022-06-27 16:46:49 +0530

    updated validation pkg list as policy-controller is getting installed in latest 1.2.0 build

--- suite/tap_test/multicluster_install_test/TAP_e2e_Install_uninstall_03_test.go
+++ suite/tap_test/multicluster_install_test/TAP_e2e_Install_uninstall_03_test.go
@@ -9,7 +9,7 @@ import (
 
 func TestTapInstallIterateProfile(t *testing.T) {
 	t.Log("************** TestCase START: TestTapInstallIterateProfile **************")
-	validatePackagesList := []string{"appliveview", "appliveview-connector", "appsso", "appliveview-conventions", "buildservice", "cartographer", "cert-manager", "cnrs", "contour", "conventions-controller", "developer-conventions", "fluxcd-source-controller", "image-policy-webhook", "ootb-delivery-basic", "ootb-supply-chain-basic", "ootb-templates",  "service-bindings", "services-toolkit", "source-controller", "spring-boot-conventions", "tap", "tap-auth", "tap-telemetry", "tekton-pipelines"}
+	validatePackagesList := []string{"appliveview", "appliveview-connector", "appsso", "appliveview-conventions", "buildservice", "cartographer", "cert-manager", "cnrs", "contour", "conventions-controller", "developer-conventions", "fluxcd-source-controller", "image-policy-webhook", "ootb-delivery-basic", "ootb-supply-chain-basic", "ootb-templates",  "policy-controller", "service-bindings", "services-toolkit", "source-controller", "spring-boot-conventions", "tap", "tap-auth", "tap-telemetry", "tekton-pipelines"}
 	testenv.Test(t,
 		common_features.InstallPackage(t, suiteConfig.Tap.Name, suiteConfig.Tap.PackageName, suiteConfig.Tap.Version, suiteConfig.Tap.Namespace, suiteConfig.Multicluster.IterateTapValuesFile, suiteConfig.Tap.PollTimeout),
 		common_features.ValidateListofInstalledPackage(t, suiteConfig.Tap.Namespace, validatePackagesList),
