a9ef90e4e9f6e20f280378bfcb2e8d73248dfc32
Madhuri Dogra <mdogra@vmware.com>
2022-06-29 07:47:08 +0000

    Install kapp controller and secretgen controller from github

--- suite/envfuncs/cluster-essentials-funcs.go
+++ suite/envfuncs/cluster-essentials-funcs.go
@@ -7,11 +7,9 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"os"
 
 	"gitlab.eng.vmware.com/tap/tap-packages/suite/pkg/kubectl/kubectl_helpers"
-	"gitlab.eng.vmware.com/tap/tap-packages/suite/pkg/pivnet/pivnet_libs"
-	"gitlab.eng.vmware.com/tap/tap-packages/suite/pkg/utils/linux_util"
+	"gitlab.eng.vmware.com/tap/tap-packages/suite/pkg/kubectl/kubectl_libs"
 	"sigs.k8s.io/e2e-framework/pkg/env"
 	"sigs.k8s.io/e2e-framework/pkg/envconf"
 )
@@ -24,37 +22,41 @@ func InstallClusterEssentials(tanzunetHost string, tanzunetApiToken string, prod
 			log.Println("kapp-controller or secretgen-controller deployment exists.")
 			return ctx, nil
 		}
-		log.Println("Download artifacts from Tanzu Network")
-		log.Println("Logging into tanzunet")
-		if !pivnet_libs.Login(tanzunetHost, tanzunetApiToken) {
-			log.Fatalln("Unable to login to tanzunet")
-		}
-		if !pivnet_libs.DownloadProductFile(productFileId, productSlug, releaseVersion) {
-			log.Fatalln("Unable to download product file")
-		}
-		extract_cluster_essentials_cmd := fmt.Sprintf("mkdir ./tanzu-cluster-essentials; tar -xvf %s -C ./tanzu-cluster-essentials", downloadBundle)
-		response, err := linux_util.ExecuteCmdInBashMode(extract_cluster_essentials_cmd)
+		// log.Println("Download artifacts from Tanzu Network")
+		// log.Println("Logging into tanzunet")
+		// if !pivnet_libs.Login(tanzunetHost, tanzunetApiToken) {
+		// 	log.Fatalln("Unable to login to tanzunet")
+		// }
+		// if !pivnet_libs.DownloadProductFile(productFileId, productSlug, releaseVersion) {
+		// 	log.Fatalln("Unable to download product file")
+		// }
+		// extract_cluster_essentials_cmd := fmt.Sprintf("mkdir ./tanzu-cluster-essentials; tar -xvf %s -C ./tanzu-cluster-essentials", downloadBundle)
+		// response, err := linux_util.ExecuteCmdInBashMode(extract_cluster_essentials_cmd)
+		// if err != nil {
+		// 	return ctx, fmt.Errorf("error while deploying cluster-essentials: %w: %s", err, response)
+		// }
+
+		// log.Println("Installing Tanzu cluster Essentials...")
+		// log.Println("Setting up required environment variables for installing Tanzu Cluster Essentials.")
+		// os.Setenv("INSTALL_BUNDLE", installBundle)
+		// log.Printf("INSTALL_BUNDLE env set to: %s", os.Getenv("INSTALL_BUNDLE"))
+		// os.Setenv("INSTALL_REGISTRY_HOSTNAME", installRegistryHostname)
+		// log.Printf("INSTALL_REGISTRY_HOSTNAME env set to: %s", os.Getenv("INSTALL_REGISTRY_HOSTNAME"))
+		// os.Setenv("INSTALL_REGISTRY_USERNAME", InstallRegistryUsername)
+		// log.Printf("INSTALL_REGISTRY_USERNAME env set to: %s", os.Getenv("INSTALL_REGISTRY_USERNAME"))
+		// os.Setenv("INSTALL_REGISTRY_PASSWORD", installRegistryPassword)
+		// log.Println("INSTALL_REGISTRY_PASSWORD env set.")
+
+		err := kubectl_libs.KubectlApplyConfiguration("resources/suite/kapp-controller.yml", "")
 		if err != nil {
-			return ctx, fmt.Errorf("error while deploying cluster-essentials: %w: %s", err, response)
+			return ctx, fmt.Errorf("error while setting up developer namespace %s", "kapp-controller")
 		}
 
-		log.Println("Installing Tanzu cluster Essentials...")
-		log.Println("Setting up required environment variables for installing Tanzu Cluster Essentials.")
-		os.Setenv("INSTALL_BUNDLE", installBundle)
-		log.Printf("INSTALL_BUNDLE env set to: %s", os.Getenv("INSTALL_BUNDLE"))
-		os.Setenv("INSTALL_REGISTRY_HOSTNAME", installRegistryHostname)
-		log.Printf("INSTALL_REGISTRY_HOSTNAME env set to: %s", os.Getenv("INSTALL_REGISTRY_HOSTNAME"))
-		os.Setenv("INSTALL_REGISTRY_USERNAME", InstallRegistryUsername)
-		log.Printf("INSTALL_REGISTRY_USERNAME env set to: %s", os.Getenv("INSTALL_REGISTRY_USERNAME"))
-		os.Setenv("INSTALL_REGISTRY_PASSWORD", installRegistryPassword)
-		log.Println("INSTALL_REGISTRY_PASSWORD env set.")
-
-		install_cmd := "cd ./tanzu-cluster-essentials; ./install.sh --yes"
-		output, err := linux_util.ExecuteCmdInBashMode(install_cmd)
-
-		if err != nil {
-			return ctx, fmt.Errorf("error while deploying cluster-essentials: %w: %s", err, output)
+		err1 := kubectl_libs.KubectlApplyConfiguration("resources/suite/secretgen-controller.yml", "")
+		if err1 != nil {
+			return ctx, fmt.Errorf("error while setting up developer namespace %s", "secretgen-controller")
 		}
+
 		log.Printf("cluster-essentials deployment successful")
 		return ctx, nil
 	}
